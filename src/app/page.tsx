e8b4b308efc319f734b8d062b1ac70bc1171954e
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-11-04 16:22:56 +0600

    shadCn setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
+
+        <SignUpButton>
+          <Button variant={"outline"}>Sign up</Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   );
 }
