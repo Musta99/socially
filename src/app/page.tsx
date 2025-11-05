65acd1d5e1d090cd5db4f1f12b50ad3111f5a5df
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-11-05 10:29:40 +0600

    feat Navbar implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-
-        <SignUpButton>
-          <Button variant={"outline"}>Sign up</Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <MoodToggle />
+      <h1>COntent goes here</h1>
     </>
   );
 }
